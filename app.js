cb5002256a30ae472f04bf78c7811cd3c1cd573c
Startan <tanxing.tx@alibaba-inc.com>
2017-07-25 12:55:19 +0800

    1.10.0

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-
+const bunyan = require("bunyan");
 const pmx = require('pmx');
 const pm2 = require("pm2");
 const Koa = require("koa");
 const koaBody = require('koa-body');
+
+let log = bunyan.createLogger({
+  name: "pm2-web-hook"
+});
 
 pmx.initModule({
 
@@ -39,156 +43,68 @@ pmx.initModule({
 
 }, function(err, conf) {
   
+  let hooks = [];
+  function updateApps () {
+    pm2.list(function (err, apps) {
+      if (err) {
+        log.error(`get apps info error:`, err);
+      } else {
+        apps.filter(app => !!app.pm2_env.env_pm2_web_hook).forEach(app => {
+          hooks[app.name] = app.pm2_env.env_pm2_web_hook;
+        });
+      }
+    });
+  }
+  
   pm2.connect(function (err) {
     if (err) {
-      console.log("pm2 connect error");
+      log.error("pm2 connect error", err);
       process.exit(2);
     }
     
-    // let _appName = conf.name;
-    let _appName = conf.target;
-    let _mode = conf.mode; // "custom" mode passing functions
-    let _tokenName = conf.token_name;  // used in "normal" mode
-    let _tokenValue = conf.token_value;  // used in "normal" mode
-    let _configFile = conf.config_file;  // used in "custom" mode
     let _port = conf.port;
-    let isConfigValid = true;
-    if (!_appName) {
-      console.log("You need to specify the target app name");
-      isConfigValid = false;
-    }
-    if (_mode === "normal" && _tokenValue === undefined) {
-      console.log("You need to specify token value");
-      isConfigValid = false;
-    }
-    if (!isConfigValid) {
-      process.exit(3);
-    }
+    let _interval = conf.interval;
     
     let server = new Koa();
   
     server.use(koaBody());
     
     server.use(ctx => {
+      ctx.response.status = 204;
+      if (ctx.method.toLowerCase() !== "post") return;
+      
       let payload = ctx.request.body;
       let doReload = false;
       
-      switch (_mode) {
+      let __config = hooks[ctx.path.replace("/", "")];
+      if (!__config) return ;
+      
+      switch (__config.mode) {
         case "custom":
-          doReload = require(_configFile)(payload, conf);
+          doReload = __config.handler(payload, conf);
           break;
         case "normal":
         default:
-          doReload = payload[_tokenName] === _tokenValue;
+          doReload = payload[__config.token_name] === __config.token_value;
           break;
       }
       
       if(doReload) {
-        pm2.pullAndReload(_appName, (err, meta) => {
+        let __appName = __config.name;
+        pm2.pullAndReload(__appName, function(err, meta) {
           if (!err && meta.rev) {
-            console.log(`${_appName} is at latest version:${meta.rev.current_revision}`);
+            log.info(`${_appName} is at latest version:${meta.rev.current_revision}`);
           } else {
-            console.log(`Pull ${_appName} failed`, 1, err, 2, meta);
+            log.error(`Pull ${_appName} failed`, 1, err, 2, meta);
           }
         });
       }
-      
-      ctx.response.status = 204;
     });
     
     server.listen(_port);
-    console.log(`Webhook server listen on ${_port}`);
+    log.info(`Webhook server listen on ${_port}`);
+    
+    setInterval(updateApps, _interval);
   });
-  
-
-  /**
-   * Module specifics like connecting to a database and
-   * displaying some metrics
-   */
-
-  /**
-   *                      Custom Metrics
-   *
-   * Let's expose some metrics that will be displayed into Keymetrics
-   *   For more documentation about metrics: http://bit.ly/1PZrMFB
-   */
-  // var Probe = pmx.probe();
-  //
-  // var value_to_inspect = 0;
-
-  /**
-   * .metric, .counter, .meter, .histogram are also available (cf doc)
-   */
-  // var val = Probe.metric({
-  //   name : 'test-probe',
-  //   value : function() {
-  //     return value_to_inspect;
-  //   },
-  //   /**
-  //    * Here we set a default value threshold, to receive a notification
-  //    * These options can be overriden via Keymetrics or via pm2
-  //    * More: http://bit.ly/1O02aap
-  //    */
-  //   alert : {
-  //     mode     : 'threshold',
-  //     value    : 20,
-  //     msg      : 'test-probe alert!',
-  //     action   : function(val) {
-  //       // Besides the automatic alert sent via Keymetrics
-  //       // You can also configure your own logic to do something
-  //       console.log('Value has reached %d', val);
-  //     }
-  //   }
-  // });
-  //
-  // setInterval(function() {
-  //   // Then we can see that this value increase over the time in Keymetrics
-  //   value_to_inspect++;
-  // }, 300);
-
-
-  /**
-   *                Simple Actions
-   *
-   *   Now let's expose some triggerable functions
-   *  Once created you can trigger this from Keymetrics
-   *
-   */
-  // pmx.action('env', function(reply) {
-  //   return reply({
-  //     env: process.env
-  //   });
-  // });
-
-  /**
-   *                 Scoped Actions
-   *
-   *     This are for long running remote function
-   * This allow also to res.emit logs to see the progress
-   *
-   **/
-  // var spawn = require('child_process').spawn;
-  //
-  // pmx.scopedAction('lsof cmd', function(options, res) {
-  //   var child = spawn('lsof', []);
-  //
-  //   child.stdout.on('data', function(chunk) {
-  //     chunk.toString().split('\n').forEach(function(line) {
-  //       /**
-  //        * Here we send logs attached to this command
-  //        */
-  //       res.send(line);
-  //     });
-  //   });
-  //
-  //   child.stdout.on('end', function(chunk) {
-  //     /**
-  //      * Then we emit end to finalize the function
-  //      */
-  //     res.end('end');
-  //   });
-  //
-  // });
-
 
 });
